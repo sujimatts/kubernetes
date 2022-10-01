f4bd6f57a3617bc306f211a98a898a9cd741c1c7
sujimatts <40743779+sujimatts@users.noreply.github.com>
2022-10-01 20:43:05 +0530

    Update main.tf

--- rke_cluster_terraform/main.tf
+++ rke_cluster_terraform/main.tf
@@ -12,12 +12,12 @@ provider "rke" {
   log_file = "rke_debug.log"
 }
 # Create a new RKE cluster using config yaml
-resource "rke_cluster" "foo" {
-  delay_on_creation = 180   
+resource "rke_cluster" "cluster" {
+  delay_on_creation = 30   
   cluster_yaml = file("cluster.yml")
 }
 
 resource "local_sensitive_file" "kube_cluster_yaml" {
-  filename = "${path.root}/kube_config_cluster.yml"
-  content  = "${rke_cluster.foo.kube_config_yaml}"
+  filename = "~/.kube/config"
+  content  = "${rke_cluster.cluster.kube_config_yaml}"
 }
